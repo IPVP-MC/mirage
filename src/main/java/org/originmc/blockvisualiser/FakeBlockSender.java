988a9b9d5cdc4ba1208455ffb6096c7cd97e0f47
Matthew <matthew.steglinski@utoronto.ca>
2016-02-11 14:50:02 -0500

    Add documentation to FakeBlockSender

--- src/main/java/org/originmc/blockvisualiser/FakeBlockSender.java
+++ src/main/java/org/originmc/blockvisualiser/FakeBlockSender.java
@@ -10,16 +10,56 @@ import java.util.function.Predicate;
 
 public interface FakeBlockSender {
 
+    /**
+     * Gets a sent {@link FakeBlock} for a player at a specific location.
+     *
+     * @param player the player
+     * @param location the location
+     * @return the sent block
+     */
     FakeBlock getBlockAt(Player player, Location location);
 
+    /**
+     * Sends a single {@link FakeBlock} to a player at a specific location. This method
+     * uses the provided {@link BlockGenerator} implementation to create fake block
+     * data which is used to create a fake block to send.
+     *
+     * @param player the player
+     * @param generator the generator
+     * @param location the location
+     */
     void sendBlock(Player player, BlockGenerator generator, Location location);
 
+    /**
+     * Sends multiple {@link FakeBlock}s to a player at given locations.
+     *
+     * @param player the player
+     * @param generator the generator
+     * @param locations the location
+     */
     void sendBlocks(Player player, BlockGenerator generator, Collection<Location> locations);
 
+    /**
+     * Reverts a fake block that was sent to a player.
+     *
+     * @param player the player
+     * @param location the location of the block
+     */
     void clearBlockAt(Player player, Location location);
 
+    /**
+     * Reverts all blocks that have been sent to a player.
+     *
+     * @param player the player
+     */
     void clearBlocks(Player player);
 
+    /**
+     * Reverts all blocks that have been sent to a player and match a condition.
+     *
+     * @param player the player
+     * @param test the condition
+     */
     void clearBlocks(Player player, Predicate<? extends FakeBlock> test);
 
 }
