404e445f8d137c2c66c5b900eb8a1100fbc89f2c
Matthew <matthew.steglinski@utoronto.ca>
2016-02-11 19:21:23 -0500

    Clear players blocks when they log out or switch worlds

--- src/main/java/org/originmc/blockvisualiser/VisualiserPlugin.java
+++ src/main/java/org/originmc/blockvisualiser/VisualiserPlugin.java
@@ -2,11 +2,15 @@ package org.originmc.blockvisualiser;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.originmc.blockvisualiser.protocol.BlockDigAdapter;
 import org.originmc.blockvisualiser.protocol.BlockPlaceAdapter;
 
-public class VisualiserPlugin extends JavaPlugin {
+public class VisualiserPlugin extends JavaPlugin implements Listener {
 
     private static FakeBlockSender blockSender = new FakeBlockSenderImpl();
 
@@ -16,11 +20,22 @@ public class VisualiserPlugin extends JavaPlugin {
         ProtocolManager manager = ProtocolLibrary.getProtocolManager();
         manager.addPacketListener(new BlockDigAdapter(this));
         manager.addPacketListener(new BlockPlaceAdapter(this));
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         getServer().getOnlinePlayers().forEach(blockSender::clearBlocks);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        blockSender.clearBlocks(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerSwitchWorld(PlayerChangedWorldEvent event) {
+        blockSender.clearBlocks(event.getPlayer());
     }
 
     /**
